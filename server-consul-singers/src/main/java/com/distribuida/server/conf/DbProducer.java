e7aa0da2eb38b069c27b9ed3e87be16a90a25807
lfpantoja <luchopantoja7@gmail.com>
2020-01-27 13:51:20 -0500

    server-consul-singers

---
+++ server-consul-singers/src/main/java/com/distribuida/server/conf/DbProducer.java
@@ -0,0 +1,71 @@
+package com.distribuida.server.conf;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+import javax.ws.rs.Path;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+//@Path(value="/")
+@ApplicationScoped
+public class DbProducer{
+	
+    private static BasicDataSource basicDataSource=null;
+    
+    @Inject
+	private Config config;
+    
+    @Inject
+	@ConfigProperty(name = "driver")
+	private String driver;
+    
+    @Inject
+	@ConfigProperty(name = "url")
+	private String url;
+    
+    @Inject
+	@ConfigProperty(name = "user")
+	private String user;
+    
+    @Inject
+	@ConfigProperty(name = "pass")
+	private String pass;
+    
+    @PostConstruct
+    private void postConstruct() {
+    	if (null==basicDataSource){
+    		basicDataSource = new BasicDataSource();
+            basicDataSource.setDriverClassName(driver);//org.postgresql.Driver
+            basicDataSource.setUsername(user);
+            basicDataSource.setPassword(pass);
+            basicDataSource.setUrl(url);//jdbc:postgresql://localhost:5432/deber04
+            basicDataSource.setMaxActive(20);
+            basicDataSource.setMinIdle(5);
+            basicDataSource.setMaxIdle(10);
+    	}
+    }
+    
+    public DbProducer(){
+         if (null==basicDataSource){
+        	 //config.getValue("user",String.class)
+//             basicDataSource = new BasicDataSource();
+//             basicDataSource.setDriverClassName(driver);//org.postgresql.Driver
+//             basicDataSource.setUsername(user);
+//             basicDataSource.setPassword(pass);
+//             basicDataSource.setUrl(url);//jdbc:postgresql://localhost:5432/deber04
+//             basicDataSource.setMaxActive(200);
+//             basicDataSource.setMinIdle(50);
+//             basicDataSource.setMaxIdle(100);
+         }
+     }
+
+    @ApplicationScoped
+    @Produces
+     public BasicDataSource getDataSource(){
+         return basicDataSource;
+     }
+}
